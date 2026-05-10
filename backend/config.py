ec9457e5d86d8180c4ad4890ec5ee522666b2f0f
Ters94 <marwaters94@gmail.com>
2026-05-10 20:58:44 +0200

    feat: add SMTP variables to config

--- backend/config.py
+++ backend/config.py
@@ -15,5 +15,12 @@ class Settings:
     #les variables d'environnement sont toujours des chaînes de caractères, donc on convertit en int
     JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES", 60))
 
+#Pour l'envoi automatique des audits par email, on récupère les paramètres SMTP depuis les variables d'environnement
+    SMTP_HOST       = os.getenv("SMTP_HOST", "smtp.gmail.com")
+    SMTP_PORT       = int(os.getenv("SMTP_PORT", 587))
+    SMTP_USER       = os.getenv("SMTP_USER", "")
+    SMTP_PASSWORD   = os.getenv("SMTP_PASSWORD", "")
+
+    APP_NAME        = os.getenv("APP_NAME", "ENGIE Queries")
 
 settings = Settings()
